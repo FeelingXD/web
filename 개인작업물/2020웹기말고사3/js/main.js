86ee7aef2b40db12287d44d9643aa8581018e8ef
jimin <45274316+FeelingXD@users.noreply.github.com>
2020-12-22 01:51:20 +0900

    push

--- 개인작업물/2020웹기말고사3/js/main.js
+++ 개인작업물/2020웹기말고사3/js/main.js
@@ -1,4 +1,6 @@
+
 window.onload=function(){
+    alert('환영합니다.');
     var AD=document.getElementsByClassName('AD_img')[0];
     //마우스 오버이벤트
     AD.addEventListener('mouseover',function(){
@@ -8,9 +10,20 @@ window.onload=function(){
     AD.addEventListener('mouseout',function(){
         
     });
-    var img = document.getElementsByTagName('article');
+    //이미지 클릭시 원본 링크 새창출력
+    var img = document.getElementsByTagName('img');
         for(var x=0;x<img.length;x++)
         {
-            img.item(x).onclick=function(){window.open(this.style.background-image)};
+            img.item(x).onclick=function(){window.open(this.src,"PopupWin", "width=500,height=600")};
+        }
+    //미구현 기능 알림
+    
+        
+   
+    
+    var a_tag= document.getElementsByTagName('a');
+    for (x=0;x<change.length;x++)
+        {
+            
         }
 }
