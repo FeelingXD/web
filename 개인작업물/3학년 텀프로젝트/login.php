33a48f90c5c2f9df868eb68e6c01dfed762c79e1
jimin <45274316+FeelingXD@users.noreply.github.com>
2021-05-12 14:32:53 +0900

    start

---
+++ 개인작업물/3학년 텀프로젝트/login.php
@@ -0,0 +1,8 @@
+<?php 
+    include 'inc_head.php';
+    ?>
+<!doctype html>
+<html lang="ko">
+<head>
+</head>
+</html>
---
+++ 개인작업물/conTestOra.php
@@ -0,0 +1,15 @@
+<?php
+    $conn = oci_connect('scott', 'tiger', 'azza.gwangju.ac.kr/orcl');
+    $stid = oci_parse($conn, 'select table_name from user_tables');
+    oci_execute($stid);
+    echo "<table>\n";
+    while (($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo " <td>".($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;")."</td>\n";
+    }
+    echo "</tr>\n";
+    }
+echo "</table>\n";
+oci_close($conn);
+?>
