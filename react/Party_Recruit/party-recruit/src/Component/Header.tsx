1da099c60fddf85cfa7d86ed0320cc9709c0e22c
jimin <wlals425315@gmail.com>
2022-10-03 05:55:59 +0900

    add reset.scss

--- react/Party_Recruit/party-recruit/src/Component/Header.tsx
+++ react/Party_Recruit/party-recruit/src/Component/Header.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import styles from "../Css/Header.module.scss"
 type Props = {}
-
+function tologin(){
+  console.log('function login test')
+}
 export default function Header({}: Props) {
   return (
     <header className={styles.header}>
       Header
       <div className={styles.menu}>
         menu
-        <div>
-            menu in div
-        </div>
+        <ul>
+          <a href="#" className={styles.link_login} onClick={tologin}>
+            <span className="text">로그인</span>
+          </a>
+        </ul>
       </div>
     </header>
   )
