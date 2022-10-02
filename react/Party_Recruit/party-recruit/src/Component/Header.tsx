17be25fb10388e1f39a43703fccd747e172681ab
jimin <wlals425315@gmail.com>
2022-10-03 03:20:26 +0900

    add scss

--- react/Party_Recruit/party-recruit/src/Component/Header.tsx
+++ react/Party_Recruit/party-recruit/src/Component/Header.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import styles from "../Css/Header.module.scss"
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <div>Header</div>
+    <header className={styles.header}>
+      Header
+      <div className='nav'>
+        nav
+        <div>
+            nav in div
+        </div>
+      </div>
+
+    </header>
   )
 }
