421d6f8065ecd16ad7799255006af4f1c0be3700
지민 <wlals425315@gmail.com>
2022-10-05 23:55:55 +0900

    express test

---
+++ react/expresstest/server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.listen(8080,function(){
+    console.log('listening complete')
+})
+
+app.use(express.static(path.join(__dirname,'../build')))
+// app.use((req,res) => {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+// })
+// app.use((req, res) => {
+//     res.header("Access-Control-Allow-Origin", "*"); // 모든 도메인 허용
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // 특정 도메인 허용
+// });
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'../build/index.html'));
+})
