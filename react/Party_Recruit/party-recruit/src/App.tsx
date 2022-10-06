3887018c74ac4ac7ca026bd3fed46c0c538a1f17
지민 <wlals425315@gmail.com>
2022-10-07 05:30:00 +0900

    add bootstrap & remove server .
    server will be on later.

--- react/Party_Recruit/party-recruit/src/App.tsx
+++ react/Party_Recruit/party-recruit/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
 import './App.css';
 import Body from './Component/Body';
 import Footer from './Component/Footer';
 import Header from './Component/Header';
+
 function App() {
   return (
     <div className="App">
