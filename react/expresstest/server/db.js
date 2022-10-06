0cb4af208358e6aacaab46c0c16800c9dea83ae1
지민 <wlals425315@gmail.com>
2022-10-07 03:50:24 +0900

    remove junk and update db.js

--- react/expresstest/server/db.js
+++ react/expresstest/server/db.js
@@ -1,21 +1,24 @@
-const mongoose = require("mongoose");
-const mongodb = "mongodb://localhost:27017/party-recruit";
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/party-recruit');
+
 var db = mongoose.connection;
-        mongoose.connect('mongodb://localhost:27017/party-recruit');
-        db.on('error', console.error);
-        db.once('open', function(){
-            console.log('connected!');
-        });
-        
-module.exports = () =>{
-    function connect(){
-        var db = mongoose.connection;
-        mongoose.connect('mongodb://localhost:27017/party-recruit');
-        db.on('error', console.error);
-        db.once('open', function(){
-            console.log('connected!');
-        });
-    }
-    connect();
-    mongoose.connection.on('disconnected',connect);
-}
+
+db.on('error', function(err){
+    console.error('err', err);
+})
+db.once('open', function() {
+    console.log('Connected!');
+});
+// module.exports = () =>{
+//     function connect(){
+//         var db = mongoose.connection;
+//         mongoose.connect('mongodb://localhost:27017/party-recruit');
+//         db.on('error', console.error);
+//         db.once('open', function(){
+//             console.log('connected!');
+//         });
+//     }
+//     connect();
+//     mongoose.connection.on('disconnected',connect);
+// }
