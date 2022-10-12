d518cac58aa61c06f270ae0681b7bec5c760bd56
지민 <wlals425315@gmail.com>
2022-10-13 01:29:04 +0900

    middleware logger

--- react/todo-server/src/app.ts
+++ react/todo-server/src/app.ts
@@ -2,8 +2,35 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyparser from 'body-parser';
 
+import { requestLoggerMiddleware } from './request.logger.middleware';
+
 const app = express();
 app.use(cors());
 app.use(bodyparser.json());
+
+
+// Todo - request logger
+app.use(requestLoggerMiddleware);
+
+
+// app.get('/todo',(req:express.Request, resp: express.Response, next: express.NextFunction)=>{
+//     resp.json([{id: 1, description:' buy bread'}]);
+// });
+
+// app.post('/todo',(req:express.Request,resp:express.Response,next:express.NextFunction)=> {
+//     console.info(req.body);
+//     resp.end();
+// });
+
+// app.put('/todo/:id',(req:express.Request,resp:express.Response,next:express.NextFunction)=>{
+//     console.info(req.body);
+//     console.info(req.params.id);
+//     resp.end();
+// });
+
+// app.delete('todo/:id',(req:express.Request,resp:express.Response,next:express.NextFunction)=>{
+//     console.info(req.params.id);
+//     resp.end();
+// });
 
 export { app };
