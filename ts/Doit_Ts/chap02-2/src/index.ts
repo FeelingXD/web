5817682ff4c3953b30ce3d65f2345a4fb909e7d2
jimin <wlals425315@gmail.com>
2022-11-07 04:20:02 +0900

    up to chap2

--- ts/Doit_Ts/chap02-2/src/index.ts
+++ ts/Doit_Ts/chap02-2/src/index.ts
@@ -1,20 +1,4 @@
-let MAX_AGE=100
-
-interface IPerson{
-    name: string,
-    age: Number
-}
-
-class Person implements IPerson{
-    constructor(public name: string,public age: number){}
-}
-
-function makeRandomNumber(max: number =MAX_AGE):number{
-    return Math.ceil((Math.random()*max))
-}
-
-const makePerson = (name: string, age:number = makeRandomNumber()) =>({name,age})
-
+import {IPerson ,makePerson} from './person/Person'
 const testMakeperson = ():void =>{
     let jane: IPerson=makePerson("Jane")
     let jack: IPerson=makePerson("Jack")
