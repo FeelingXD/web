e615a7be7517dec5f440b2cc1716ceca227b997a
지민 <wlals425315@gmail.com>
2022-11-16 18:07:22 +0900

    fix

---
+++ ts/nomadcoder/Typescript/class/interface.ts
@@ -0,0 +1,31 @@
+export
+type Team = "red"|"blue"|"yellow"
+type Health = 1|5|10
+
+
+// type Player={
+//     nickname :string,
+//     team:Team,
+//     health :Health
+// }
+
+interface Person{
+    nickname: string,
+    team:Team,
+    health:Health
+}
+
+//
+
+interface User{
+    name:string
+}
+
+interface Player extends User{
+
+}
+const nico :Player={
+    name:"nico"
+}
+
+console.log(nico)
