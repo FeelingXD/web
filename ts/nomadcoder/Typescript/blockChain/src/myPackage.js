8158e4802b58e3990fd65dcef4f9a3340106f70f
지민 <wlals425315@gmail.com>
2022-11-17 13:59:38 +0900

    nomad -typescript

---
+++ ts/nomadcoder/Typescript/blockChain/src/myPackage.js
@@ -0,0 +1,19 @@
+
+// 
+/**  initialize program
+* @param {object} config
+* @param {boolean} config.debug
+* @param {string} config.url
+* @returns {void}
+*/
+export function init(config){
+    return true;
+}
+/**
+ * exit program
+ * @param {number} code 
+ * @returns {number}
+ */
+export function exit(code){
+    return code+1
+}
