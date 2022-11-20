182d7c239fdfdd272d845cc6741f7f9bd34c1334
jimin <wlals425315@gmail.com>
2022-11-20 19:05:26 +0900

    Update add.ts

--- script/typescript/Doit_Ts/function/add.ts
+++ script/typescript/Doit_Ts/function/add.ts
@@ -1,10 +1,15 @@
 export type NumberToNumberFunc = (arg0: number) =>number
 export const add =(a:number):NumberToNumberFunc =>{
+    
     const _add:NumberToNumberFunc = (b:number):number =>{
-        console.log(a)
-        return a+b
+        return a=a+b
     }
     return _add
 }
-console.log(add(1)(2))
+
 let plus1=add(1)
+console.log(plus1(2))
+console.log(plus1(2))
+console.log(plus1(2))
+console.log(plus1(2))
+console.log(plus1(2))
