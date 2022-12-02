8b8752770df7f6e8809fe7180c7df279a504fff4
지민 <wlals425315@gmail.com>
2022-12-02 16:46:11 +0900

    Refactoring - prettier code form

--- react/Note/src/components/NoteForm.tsx
+++ react/Note/src/components/NoteForm.tsx
@@ -1,100 +1,99 @@
-import { Form, Stack, Row, Col, Button, NavLink } from "react-bootstrap"
-import CreatableSelect from 'react-select/creatable';
-import { Link, useNavigate } from "react-router-dom"
+import { Form, Stack, Row, Col, Button, NavLink } from "react-bootstrap";
+import CreatableSelect from "react-select/creatable";
+import { Link, useNavigate } from "react-router-dom";
 import { FormEvent, useRef, useState } from "react";
 import { NoteData, Tag } from "../App";
-import { v4 as uuidV4 } from 'uuid';
+import { v4 as uuidV4 } from "uuid";
 
 type NoteFormProps = {
-    onSubmit: (data: NoteData) => void,
-    onAddTag: (tag: Tag) => void
-    availableTags: Tag[]
-} & Partial<NoteData>
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+} & Partial<NoteData>;
 
 export const NoteForm = ({
-    onSubmit,
-    onAddTag,
-    availableTags,
-    title = "",
-    markdown = "",
-    tags = []
+  onSubmit,
+  onAddTag,
+  availableTags,
+  title = "",
+  markdown = "",
+  tags = [],
 }: NoteFormProps) => {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLInputElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
+  const navigate = useNavigate();
 
-    const titleRef = useRef<HTMLInputElement>(null)
-    const markdownRef = useRef<HTMLInputElement>(null)
-    const [selectedTags, setSelectedTags] = useState<Tag[]>(tags)
-    const navigate = useNavigate()
+  const hanndleSubmit = (e: FormEvent) => {
+    e.preventDefault();
 
-    const hanndleSubmit = (e: FormEvent) => {
-        e.preventDefault()
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: selectedTags,
+    });
 
-        onSubmit({
-            title: titleRef.current!.value,
-            markdown: markdownRef.current!.value,
-            tags: selectedTags,
-        })
+    navigate("..");
+  };
 
-        navigate('..')
-    }
-
-    return (
-        <Form onSubmit={hanndleSubmit}>
-            <Stack gap={4}>
-                <Row>
-                    <Col>
-                        <Form.Group controlId="title">
-                            <Form.Label>Title</Form.Label>
-                            <Form.Control ref={titleRef} required />
-                        </Form.Group>
-                    </Col>
-                    <Col>
-                        <Form.Group controlId="tags">
-                            <Form.Label>Tags</Form.Label>
-                            <CreatableSelect
-                                onCreateOption={label => {
-                                    const newTag = { id: uuidV4(), label }
-                                    onAddTag(newTag)
-                                    setSelectedTags(prev => [...prev, newTag])
-                                }}
-
-                                value={selectedTags.map(tag => {
-                                    return { label: tag.label, value: tag.id }
-                                })}
-
-                                options={availableTags.map(tag => {
-                                    return { label: tag.label, value: tag.id }
-                                })}
-
-                                onChange={tags => {
-                                    setSelectedTags(
-                                        tags.map(tag => {
-                                            return { label: tag.label, id: tag.value }
-                                        })
-                                    )
-                                }}
-                                isMulti
-                            />
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Row>
-                    <Form.Group controlId="markdown">
-                        <Form.Label>Body</Form.Label>
-                        <Form.Control
-                        ref={markdownRef}
-                        required as="textarea"
-                        defaultValue={markdown}
-                        rows={15} />
-                    </Form.Group>
-                </Row>
-                <Stack direction="horizontal" className="justify-content-end" gap={2}>
-                    <Button type="submit">Save</Button>
-                    <Link to={".."}>
-                        <Button type="button" variant="secondary">Cancel</Button>
-                    </Link>
-                </Stack>
-            </Stack>
-        </Form>
-
-    )
-}
+  return (
+    <Form onSubmit={hanndleSubmit}>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control ref={titleRef} required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableSelect
+                onCreateOption={(label) => {
+                  const newTag = { id: uuidV4(), label };
+                  onAddTag(newTag);
+                  setSelectedTags((prev) => [...prev, newTag]);
+                }}
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                isMulti
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          <Form.Group controlId="markdown">
+            <Form.Label>Body</Form.Label>
+            <Form.Control
+              ref={markdownRef}
+              required
+              as="textarea"
+              defaultValue={markdown}
+              rows={15}
+            />
+          </Form.Group>
+        </Row>
+        <Stack direction="horizontal" className="justify-content-end" gap={2}>
+          <Button type="submit">Save</Button>
+          <Link to={".."}>
+            <Button type="button" variant="secondary">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
+    </Form>
+  );
+};
